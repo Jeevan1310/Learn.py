634b4d30716dc0b8ad9f8cb1058d3a6b336500a4
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2021-07-06 19:19:49 +0530

    fourth commit

--- pattern.py
+++ pattern.py
@@ -8,4 +8,4 @@ for i in range(rows, 0, -1):
     for j in range(0, i - 1):
         print("#", end=' ')
     print("\r")
-#a python program to print a pattern
+#its a python program to print patterns
